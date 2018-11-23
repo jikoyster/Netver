fcc0731cfa49dddf63eba39ac69fd775c1cf864e
Reynald <rynzco@gmail.com>
2018-11-23 23:52:26 +0800

    include genheader and genTopBar on template

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,63 +1,10 @@
 @include('genheader');
 
-	<!-- begin::Body -->
-	<body class="m-page--fluid m--skin- m-content--skin-light2 m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--fixed m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default">
-
 		<!-- begin:: Page -->
 		<div class="m-grid m-grid--hor m-grid--root m-page">
 
 			<!-- BEGIN: Header -->
-			<header id="m_header" class="m-grid__item    m-header " m-minimize-offset="200" m-minimize-mobile-offset="200">
-				<div class="m-container m-container--fluid m-container--full-height">
-					<div class="m-stack m-stack--ver m-stack--desktop">
-
-						<!-- BEGIN: Brand -->
-						<div class="m-stack__item m-brand  m-brand--skin-dark ">
-							<div class="m-stack m-stack--ver m-stack--general">
-								<div class="m-stack__item m-stack__item--middle m-brand__logo">
-									<a href="/" class="m-brand__logo-wrapper">
-										<img alt="" src="../assets/doctracc/assets/demo/default/media/img/logo/logo_default_dark.png" />
-									</a>
-								</div>
-								<div class="m-stack__item m-stack__item--middle m-brand__tools">
-
-									<!-- BEGIN: Left Aside Minimize Toggle -->
-									<a href="javascript:;" id="m_aside_left_minimize_toggle" class="m-brand__icon m-brand__toggler m-brand__toggler--left m--visible-desktop-inline-block  ">
-										<span></span>
-									</a>
-
-									<!-- END -->
-
-									<!-- BEGIN: Responsive Aside Left Menu Toggler -->
-									<a href="javascript:;" id="m_aside_left_offcanvas_toggle" class="m-brand__icon m-brand__toggler m-brand__toggler--left m--visible-tablet-and-mobile-inline-block">
-										<span></span>
-									</a>
-
-									<!-- END -->
-
-									<!-- BEGIN: Responsive Header Menu Toggler -->
-									<a id="m_aside_header_menu_mobile_toggle" href="javascript:;" class="m-brand__icon m-brand__toggler m--visible-tablet-and-mobile-inline-block">
-										<span></span>
-									</a>
-
-									<!-- END -->
-
-									<!-- BEGIN: Topbar Toggler -->
-									<a id="m_aside_header_topbar_mobile_toggle" href="javascript:;" class="m-brand__icon m--visible-tablet-and-mobile-inline-block">
-										<i class="flaticon-more"></i>
-									</a>
-
-									<!-- BEGIN: Topbar Toggler -->
-								</div>
-							</div>
-						</div>
-
-						<!-- END: Brand -->
-						@include('genTopBar')
-					</div>
-				</div>
-			</header>
-
+			@include('genTopBar')
 			<!-- END: Header -->
 
 			<!-- begin::Body -->
