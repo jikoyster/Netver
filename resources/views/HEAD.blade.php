2e6f1c71b31d45d3c472c83e094d8e35a760ab08
Reynald <rynzco@gmail.com>
2018-11-24 01:02:04 +0800

    general template includes topbar and sidebar; also includes head tag

---
+++ resources/views/HEAD.blade.php
@@ -0,0 +1,15 @@
+@include('genheader');
+
+		<!-- begin:: Page -->
+		<div class="m-grid m-grid--hor m-grid--root m-page">
+
+			<!-- BEGIN: Header -->
+			@include('genTopBar')
+			<!-- END: Header -->
+
+			<!-- begin::Body -->
+			<div class="m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-body">
+
+				<!-- BEGIN: Left Aside -->
+				@include('gensidebar')
+				<!-- END: Left Aside -->
