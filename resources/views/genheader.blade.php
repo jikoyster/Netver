2cc06da13088320c3d9440c5b72722d808f9dcc9
Reynald <rynzco@gmail.com>
2018-11-23 23:50:51 +0800

    separate template for TopBar and Header

--- resources/views/genheader.blade.php
+++ resources/views/genheader.blade.php
@@ -46,3 +46,6 @@
     </head>
 
     <!-- end::Head -->
+
+    <!-- begin::Body -->
+	<body class="m-page--fluid m--skin- m-content--skin-light2 m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--fixed m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default">
